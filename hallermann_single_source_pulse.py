0138f3d677697308c51aab54f57719b72d5a5a15
Torbjørn <torgje@lavabit.com>
2020-09-21 16:57:05 +0200

    Setting up separate file for hallermann pyramidal cell simulations

---
+++ hallermann_single_source_pulse.py
@@ -0,0 +1,10 @@
+from main import ExternalPotentialSim
+from parameters import (monophasic_pulse_params, cellsim_Hallermann_params)
+import numpy as np
+import neuron
+import matplotlib.pyplot as plt
+import matplotlib
+import LFPy
+import os
+from os.path import join
+import sys
