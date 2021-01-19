5bc1f58d02e36e96c8ae9c3e38fc6ee1182fec2a
tlgjerberg <tlgjerberg@protonmail.com>
2021-01-19 09:49:19 +0100

    Preparing for new test branch

--- init_params.py
+++ init_params.py
@@ -1,5 +1,15 @@
 
 init_params = {
-    'nr_of_cells': 1,
+    'current_amps': current_amps,
+    'electrode_pos': positions,
+    'measure_coords': measure_coordinates,
+    'passive': False
+}
 
+init_params_multi_cells = {
+    'nr_of_cells': 1,
+    'current_amps': current_amps,
+    'electrode_pos': positions,
+    'measure_coords': measure_coordinates,
+    'passive': False
 }
