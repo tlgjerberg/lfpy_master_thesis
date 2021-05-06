944309cdb8f555b1938e70d036838e1426abff2d
tlgjerberg <tlgjerberg@protonmail.com>
2021-05-06 16:50:57 +0200

    Added steady state potential, gathering it at root and plotting against distance

--- axon_single_source_pulse.py
+++ axon_single_source_pulse.py
@@ -33,7 +33,8 @@ current_amps = [-1e4]  # uA
 elec_positions = np.array([[0, 0, -50],
                            [0, 0, -100],
                            [0, 0, -200],
-                           [0, 0, -400]], dtype=float)
+                           [0, 0, -400],
+                           [0, 0, -800]], dtype=float)
 
 measure_coordinates = np.array(
     [[0, 0, 0], [0, 0, 300], [0, 0, 600], [0, 0, 1000]])
@@ -59,31 +60,47 @@ def run_axon(cell_models_folder, measure_coords, I, pos, z, run_sim=False, plot_
     if plot_sim:
 
         cell_tvec = np.load(
-            join(extPotSim.save_folder, f'axon_x_shift=0_z_rot={z}_{I}mA_elec_pos={pos[0]}_{pos[1]}_{pos[2]}_tvec' + '.npy'))
+            join(extPotSim.save_folder, f'axon_x_shift=0_z_rot={z:.2f}_{I}mA_elec_pos={pos[0]}_{pos[1]}_{pos[2]}_tvec' + '.npy'))
         cell_vmem = np.load(
-            join(extPotSim.save_folder, f'axon_x_shift=0_z_rot={z}_{I}mA_elec_pos={pos[0]}_{pos[1]}_{pos[2]}_vmem' + '.npy'))
+            join(extPotSim.save_folder, f'axon_x_shift=0_z_rot={z:.2f}_{I}mA_elec_pos={pos[0]}_{pos[1]}_{pos[2]}_vmem' + '.npy'))
         plotSim = PlotSimulations(
             cellsim_bisc_stick_params, monophasic_pulse_params, cell_vmem, cell_tvec)
         cell = plotSim.return_cell(cell_models_folder)
-        plotSim.plot_cellsim(cell, measure_coords,
-                             cell_vmem, cell_tvec, z, [0.05, 0.05, 0.3, 0.90])
+        plotSim.plot_cellsim(cell, measure_coords, z, [0.05, 0.05, 0.3, 0.90])
+
+        v_ss = extPotSim.find_steady_state_pot(cell_vmem)
     else:
         print('No plots generated!')
+
+    return v_ss
 
 
 start = time.time()
 z = np.pi
 task_idx = -1
+v_ss = None
 for I in current_amps:
     for pos in elec_positions:
         task_idx += 1
         if not divmod(task_idx, SIZE)[1] == RANK:
             continue
 
-        run_axon(cell_models_folder,
-                 measure_coordinates, I, pos, z, True, True)
+        v_ss = run_axon(cell_models_folder,
+                        measure_coordinates, I, pos, z, False, True)
+
         print("RANK %d doing task %d" % (RANK, task_idx))
 
+# Gathering steady state membrane potential at root
+v_ss = COMM.gather(v_ss, root=0)
+
+if RANK == 0:
+    assert v_ss[0] != None
+
+    plotSim = PlotSimulations(
+        cellsim_bisc_stick_params, monophasic_pulse_params)
+    cell = plotSim.return_cell(cell_models_folder)
+    dV = plotSim.dV(v_ss)
+    plotSim.plot_dV(elec_positions, dV)
 
 end = time.time()
 print(f'Time to execute {end - start} seconds')
