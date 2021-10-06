9ce53827cfe25f16044ea656b42e3d25759ad59e
tlgjerberg <tlgjerberg@protonmail.com>
2021-10-06 16:18:03 +0200

    Changed to work with new version of the main code

--- hallermann_cell.py
+++ hallermann_cell.py
@@ -1,7 +1,8 @@
-from main import ExternalPotentialSim
-from plotting import PlotSimulations
+from plotting import PlotSimulation
+from neurosim import NeuronSimulation
 from set_electrode_position import set_electrode_pos
 from parameters import (monophasic_pulse_params, cellsim_Hallermann_params)
+from os.path import join
 import numpy as np
 import neuron
 import matplotlib.pyplot as plt
@@ -14,16 +15,19 @@ import time
 
 cell_models_folder = join(os.path.dirname(__file__), "cell_models")
 cellsim_Hallermann_params['save_folder_name'] = 'data/Hallermann_ext_stim/no_field'
+save_folder = cellsim_Hallermann_params['save_folder_name']
 
 
 cell_rot = [np.pi, (1 / 2) * np.pi]
 for z_rot in cell_rot:
     cellsim_Hallermann_params['z_rot'] = z_rot
-    plotSim = PlotSimulations(
-        cellsim_Hallermann_params, monophasic_pulse_params)
-    cell = plotSim.return_cell(cell_models_folder)
-    # plotSim.morphology_3D(cell)
+
+    neuroSim = NeuronSimulation(cellsim_Hallermann_params)
+    cell = neuroSim.return_cell(cell_models_folder)
+    plotSim = PlotSimulation(save_folder)
+
     fig = plt.figure()
-    plotSim.xlim = [-500, 500]
-    plotSim.ylim = [-300, 1200]
-    plotSim.plot_morphology(cell, fig)
+    xlim = [-600, 600]
+    ylim = [-400, 1200]
+    plotSim.plot_morphology(cell, fig, xlim, ylim)
+    fig.savefig(join(save_folder, f'hallermann_cell_z_rot={z_rot:.2f}.png'))
